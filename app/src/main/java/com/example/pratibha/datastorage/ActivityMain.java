7a875bdcb27af4b76cda74c40dbae919e365cfdd
koushik10 <skoushik2208@gmail.com>
2016-10-12 15:38:35 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pratibha/datastorage/ActivityMain.java
@@ -0,0 +1,67 @@
+package com.example.pratibha.datastorage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+public class ActivityMain extends AppCompatActivity {
+
+    private TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        info = (TextView) findViewById(R.id.info);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            InputStream in = openFileInput(Preference.STORE_PREFERENCES);
+            if(in != null) {
+                InputStreamReader tmp=new InputStreamReader(in);
+                BufferedReader reader=new BufferedReader(tmp);
+                String str;
+                StringBuilder buf=new StringBuilder();
+                while((str=reader.readLine())!=null) {
+                    buf.append(str +"\n");
+                }
+                in.close();
+                info.setText(buf.toString());
+                info.setVisibility(View.VISIBLE);
+            } else {
+                info.setVisibility(View.INVISIBLE);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            info.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+    public void onclickPreference(View v) {
+        Intent intent = new Intent(this, Preference.class);
+        startActivity(intent);
+    }
+
+
+    public void onclickSQLite(View v) {
+        Intent intent = new Intent(this, SQLite.class);
+        startActivity(intent);
+    }
+
+
+    public void closeApp(View v) {
+        ActivityMain.this.finish();
+        System.exit(0);
+    }
+}
---
+++ app/src/main/java/com/example/pratibha/datastorage/Preference.java
@@ -0,0 +1,94 @@
+package com.example.pratibha.datastorage;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Pratibha on 9/26/2016.
+ */
+
+public class Preference extends AppCompatActivity {
+
+    private EditText bookName;
+    private EditText authorName;
+    private EditText desc;
+    private int counter = 0;
+    private SimpleDateFormat s = new SimpleDateFormat("MM/dd/yyyy-hh:mm a");
+    public final static String STORE_PREFERENCES= "assignment4_pref.txt";
+    public final static String BOOK_PREFERCENS = "assignment4_books.text"; //File to save book title
+    private final static String COUNTER = "COUNTER";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preference_activity);
+        bookName = (EditText) findViewById(R.id.book_name_text);
+        authorName = (EditText) findViewById(R.id.book_name_text);
+        desc = (EditText) findViewById(R.id.desc_text);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        counter = sharedPrefs.getInt(COUNTER, 0);
+    }
+
+    /**
+     * Helper function to save preference
+     * @param v
+     */
+    public void savePreference(View v) {
+        String name = bookName.getText().toString();
+        String author = authorName.getText().toString();
+        String description = desc.getText().toString();
+        if ((name != null) && (author != null) && (description != null)) {
+            try {
+                counter += 1;
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(COUNTER, counter);
+                editor.commit();
+                //Share preference
+                OutputStreamWriter out=new OutputStreamWriter(openFileOutput(STORE_PREFERENCES, MODE_APPEND));
+                String message = "\nSaved Preference " + counter + ", " + s.format(new Date());
+                out.write(message);
+                out.close();
+                //Book preferences
+                OutputStreamWriter out1 =new OutputStreamWriter(openFileOutput(BOOK_PREFERCENS, MODE_APPEND));
+                String book = "\n" + name + ", " + author + ". Desc: " + description;
+                out1.write(book);
+                out1.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(this, ActivityMain.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else{
+            Toast.makeText(getApplicationContext(), "Please enter valid info.",Toast.LENGTH_LONG).show();
+            return;
+        }
+    }
+
+    /**
+     * Function to cancel activity
+     * @param v
+     */
+    public void cancelActivity(View v) {
+        Intent intent = new Intent(this, ActivityMain.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
+
